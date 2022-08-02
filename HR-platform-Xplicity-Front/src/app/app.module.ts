0d29250cb775d60edc0ce65a9a192afdb862e40f
PauliusCiganas <paulius.ciganas@outlook.com>
2022-08-02 10:07:25 +0300

    primeNG config

--- HR-platform-Xplicity-Front/src/app/app.module.ts
+++ HR-platform-Xplicity-Front/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { TablePageComponent } from './pages/table-page/table-page.component';
 import { TableNewEntryPageComponent } from './pages/table-new-entry-page/table-new-entry-page.component';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { TableNewEntryPageComponent } from './pages/table-new-entry-page/table-n
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
