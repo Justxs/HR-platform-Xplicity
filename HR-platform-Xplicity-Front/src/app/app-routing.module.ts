273fb338c42168a3e925b290721d1e89781d5780
PauliusCiganas <paulius.ciganas@outlook.com>
2022-08-02 09:47:25 +0300

    App routing config

--- HR-platform-Xplicity-Front/src/app/app-routing.module.ts
+++ HR-platform-Xplicity-Front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'
+import { TableNewEntryPageComponent } from './pages/table-new-entry-page/table-new-entry-page.component';
+import { TablePageComponent } from './pages/table-page/table-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'new-entry', component: TableNewEntryPageComponent},
+  {path: 'table', component: TablePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
