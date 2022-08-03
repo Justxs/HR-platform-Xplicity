202b93b90b688bd4590e70987cca48f3a4a590b5
Justas Pranauskis <justas.pranauskis@ktu.edu>
2022-08-03 11:47:53 +0300

    Models and Services folder creation

---
+++ HR-platform-Xplicity-Front/src/app/Models/candidate.ts
@@ -0,0 +1,10 @@
+export class Candidate{
+    DateOfPastCall = ""; // add model for technology? make it as an array?
+    FirstName = "";
+    LastName = "";
+    LinkedIn = "";
+    Comment = "";
+    Technologies = ""; // add model for technology? make it as an array?
+    DateOfFutureCall = "";
+    OpenForSuggestions?: boolean;
+}
