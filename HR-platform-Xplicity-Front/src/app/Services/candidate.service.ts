4a0706b2ae8e7cff5ee4b33e9aa19d273b7e4b1d
Justas Pranauskis <justas.pranauskis@ktu.edu>
2022-08-04 01:26:23 +0300

    Tried to add data to table from API
    Need some tweaking because it doent show in table

--- HR-platform-Xplicity-Front/src/app/Services/candidate.service.ts
+++ HR-platform-Xplicity-Front/src/app/Services/candidate.service.ts
@@ -1,28 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Candidate } from '../Models/candidate';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateService {
+  private readonly candidateApi = `${environment.webApiUrl}/Candidates`;
 
-  constructor() { }
-  public getCandidate() : Candidate[] {
-    let candidate = new Candidate();
-    candidate.DateOfFutureCall = "test";
-    candidate.DatesOfPastCalls = "test";
-    candidate.FirstName = "test";
-    candidate.LastName = "test";
-    candidate.LinkedIn = "test";
-    candidate.Technologies = "test";
-    candidate.Comment = "test";
-    candidate.OpenForSuggestions = true;
-    let candidates = [];
-    candidates.push(candidate);
-    candidates.push(candidate);
-    candidates.push(candidate);
-    candidates.push(candidate);
-    candidates.push(candidate);
-    return candidates;
+  constructor(private http: HttpClient) { }
+
+  public getCandidate() : Observable<Candidate[]> {
+
+    return this.http.get<Candidate[]>(this.candidateApi);
   }
 }
--- HR-platform-Xplicity-Front/src/app/app.module.ts
+++ HR-platform-Xplicity-Front/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
@@ -38,7 +39,8 @@ import {TableModule} from 'primeng/table';
     CheckboxModule,
     InputTextareaModule,
     MultiSelectModule,
-    TableModule
+    TableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
