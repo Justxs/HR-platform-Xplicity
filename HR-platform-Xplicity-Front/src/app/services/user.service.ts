1ee901ef5599f9d5d3699b251bde03028393b38f
PauliusCiganas <paulius.ciganas@outlook.com>
2022-08-12 10:24:45 +0300

    asdf

--- HR-platform-Xplicity-Front/src/app/services/user.service.ts
+++ HR-platform-Xplicity-Front/src/app/services/user.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
-import { User } from '../Models/user';
+import { User } from '../models/user';
+
 
 @Injectable({
   providedIn: 'root'
